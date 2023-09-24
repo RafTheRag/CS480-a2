269348ba878f8a8fd55450b56702f9e7a835a90a
Myato <mattmdj77@gmail.com>
2023-09-23 23:54:17 -0700

    Update readlines.cpp

---
+++ readlines.cpp
@@ -0,0 +1,34 @@
+#include "readlines.h" 
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+void* readlines(void* arg){
+    
+    SHARED_DATA *accessData = (SHARED_DATA*)arg;
+    
+    const char* testFileName = accessData->fileName[TESTFILEINDEX];
+    accessData->lineCountInFile[TESTFILEINDEX] = 0;
+    
+    //Preperation for reading in test file lines.
+    std::ifstream testIn(testFileName);
+    std::string testLine;
+
+    //locks access to queue while testfile is still being read
+    pthread_mutex_lock(&(accessData->queue_mutex));
+
+
+    while(std::getline(testIn, testLine)){
+
+        std::transform(testLine.begin(), testLine.end(), testLine.begin(), ::tolower);
+        accessData->lineQueue.push(testLine);
+        accessData->lineCountInFile[TESTFILEINDEX]++;
+    }
+    
+    //unlocks access to queue after being read
+    pthread_mutex_unlock(&(accessData->queue_mutex));
+
+    accessData->taskCompleted[TESTFILEINDEX] = true;
+
+    pthread_exit(NULL);
+}
