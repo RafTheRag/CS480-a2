7ae6aa74c62676bebeafc413c52d37fc29b9f68b
RafTheRag <Ramirezrafael293@icloud.com>
2023-09-23 23:10:48 -0700

    WIP: Rought Implementation, Missing Trie Deallocation

---
+++ countvocabstrings.cpp
@@ -0,0 +1,40 @@
+#include "countvocabstrings.h" 
+#include "tree.h"
+#include "shared.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <iostream>
+
+void* countvocabstrings(void* arg){
+    SHARED_DATA *accessData = (SHARED_DATA*)arg;
+    std::queue<std::string> lineQueue = accessData->lineQueue;
+
+    while(!accessData->taskCompleted[VOCABFILEINDEX] || lineQueue.empty()){
+        sleep(0);
+    }
+
+    while(!lineQueue.empty()){
+        
+        int vocabCount = 0;
+        CharTrie charTrie;
+        const char* frontOfQueue = lineQueue.front().c_str();
+        charTrie.insert(frontOfQueue);
+
+        for(auto &vocab: accessData->vocabVect){
+            if(charTrie.search(vocab.c_str())){
+                vocabCount++;
+            }
+            
+        }
+
+        //charTrie.~CharTrie(); Deallocated Trie with deconstructer
+        if(vocabCount >= accessData->minNumOfVocabStringsContainedForPrinting){
+            std::cout << vocabCount << std::endl;
+        }
+
+    }
+
+
+
+
+}
